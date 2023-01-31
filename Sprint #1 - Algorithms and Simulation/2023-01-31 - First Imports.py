d4219ae612a52ef840f67d1d9941cec508e372f8
Brian Blais <bblais@gmail.com>
2023-01-31 13:26:16 -0500

    class notebooks first

---
+++ Sprint #1 - Algorithms and Simulation/2023-01-31 - First Imports.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from pylab import *
+
+
+# In[4]:
+
+
+from numpy import sin, cos
+
+
+# In[8]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+sin(90)
+
+
+# In[5]:
+
+
+from numpy import *
+
+
+# In[7]:
+
+
+sin(radians(90))
+
+
+# In[9]:
+
+
+np.sin(np.radians(90))
+
+
+# In[11]:
+
+
+linspace(2,10,23)
+
+
+# In[12]:
+
+
+x=linspace(2,10,23)
+
+
+# In[13]:
+
+
+x
+
+
+# In[14]:
+
+
+5*x
+
+
+# In[16]:
+
+
+x=linspace(2,10,23)
+y=2*x+3
+plot(x,y,'-o')
+
+
+# In[24]:
+
+
+x=linspace(2,10,500)
+y=5*x**2-2*x+3
+plot(x,y,'-',label='$5x^2-2x+3$')
+
+plot(x,2*y,'-',label='$10x^2-4x+6$')
+xlabel('Time')
+ylabel('Bacon')
+legend()
+
+
+# In[ ]:
+
+
+
+
---
+++ Sprint #1 - Algorithms and Simulation/2023-01-31 - My Not New Project.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+5+5
+
+
+# In[2]:
+
+
+a=8
+
+
+# In[3]:
+
+
+b=10
+
+
+# In[4]:
+
+
+a+b
+
+
+# In[5]:
+
+
+784634738937363*746464647484
+
+
+# # Sum all the numbers from 1 to 10
+# 
+# ## subheading
+# 
+# ### subsub heading
+# 
+# 
+# 
+# 
+# $$
+# \sum_{i=1}{10} i
+# $$
+
+# - this
+# - is 
+# - a
+# - bulleted
+# - list
+# 
+# https://www.markdownguide.org/basic-syntax/
+
+# This is **bold** or *italic*
+
+# In[6]:
+
+
+S=1+2+3+4+5+6+7+8+9+10
+
+
+# In[7]:
+
+
+S
+
+
+# In[8]:
+
+
+print("one")
+for i in range(10):
+    print(i)
+    print("here")
+    
+print("two")
+
+
+# In[9]:
+
+
+print("one")
+for bob in range(10):
+    print(i)
+    print("here")
+    
+print("two")
+
+
+# In[10]:
+
+
+print("one")
+for bob in range(10):
+    print(BOB)
+    print("here")
+    
+print("two")
+
+
+# In[11]:
+
+
+print("one")
+for bob in range(10):
+    print(bob)
+    print("here")
+    
+print("two")
+
+
+# In[12]:
+
+
+a=5
+a=a+6
+
+
+# In[13]:
+
+
+a
+
+
+# In[14]:
+
+
+S=0
+n=1
+for i in range(10):
+    S=S+n
+    print(n,S)
+    n=n+1
+
+
+# In[15]:
+
+
+S=0
+n=1
+for i in range(100):
+    S=S+n
+    n=n+1
+
+
+# In[16]:
+
+
+S
+
+
+# what is the factorial of 100?
+
+# In[17]:
+
+
+S=1
+n=1
+for i in range(100):
+    S=S*n
+    n=n+1
+
+
+# In[18]:
+
+
+S
+
+
+# In[21]:
+
+
+def factorial(N):
+    S=1
+    n=1
+    for i in range(N):
+        S=S*n
+        n=n+1
+        
+    return S
+        
+
+
+# In[23]:
+
+
+factorial(100)
+
+
+# In[ ]:
+
+
+
+
