4b4c25692904c55fa1e004790a3abbf13b670a39
Brian Blais <bblais@gmail.com>
2023-02-02 13:35:44 -0500

    more class

--- Sprint #1 - Algorithms and Simulation/2023-02-01 - Taking Derivatives.py
+++ Sprint #1 - Algorithms and Simulation/2023-02-01 - Taking Derivatives.py
@@ -123,18 +123,51 @@ plot(x_expected,y_expected,'r--')
 # - run through each x value
 # - get the function a little bit to the left and to the right
 # - calculate the area -- rectangle and a triangle
+# - keep a running sum
 # - keep a record of each of these calculation (collecting all of the individual calc into array)
 
-# In[ ]:
+# In[17]:
 
 
+x_start=-5
+x_end=15
+step=0.1
+
+S=Storage()
+
+x=x_start
+total_area=0
+while x<=x_end:
+        
+    y_left=fun(x-step/2)
+    y_right=fun(x+step/2)
+    
+    area_rect=y_right*step
+    area_tri=(1/2)*step*(y_left-y_right)
+    
+    total_area=total_area+area_rect+area_tri
+    
+    
+    S+=x,total_area   # saving the result for later
+    
+    x=x+step
+    
+print("done.")
+
+x,total_area=S.arrays()  # give me the saved results as two arrays
 
 
-
-# In[ ]:
-
+# In[19]:
 
 
+figure(figsize=(6,4))
+
+plot(x,total_area,'b-')  # the numerical derivative
+
+xx=x_expected=linspace(-5,15,100)
+y_expected=xx**3/3 + 10*xx**2/2 + 11*xx -5  # this is the expected from calculus
+
+plot(x_expected,y_expected,'r--')
 
 
 # In[ ]:
@@ -157,16 +190,45 @@ plot(x_expected,y_expected,'r--')
 # - check the sign -- if signs are the same, then if we multiply them we get 1, if prod signs not equal 1, then zero
 # - keep a record of each of these calculation (collecting all of the individual calc into array)
 
-# In[15]:
+# In[20]:
 
 
 sign(-5)
 
 
-# In[16]:
+# In[21]:
 
 
 sign(9)
+
+
+# In[24]:
+
+
+x_start=-5
+x_end=15
+step=0.01
+
+S=Storage()
+
+x=x_start
+while x<=x_end:
+    
+    # print(x," ",end="")
+    
+    y_left=fun(x-step/2)
+    y_right=fun(x+step/2)
+    
+    if sign(y_left)!=sign(y_right):
+        S+=x,y_left, y_right  # saving the result for later
+    
+    x=x+step
+    
+print("done.")
+
+x,y_left, y_right=S.arrays()  # give me the saved results as two arrays
+
+x,y_left, y_right
 
 
 # In[ ]:
