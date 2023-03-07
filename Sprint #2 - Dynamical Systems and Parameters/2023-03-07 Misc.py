03be1e235f6036a3564b0f469876b0802c88720c
Brian Blais <bblais@gmail.com>
2023-03-07 13:04:35 -0500

    class

---
+++ Sprint #2 - Dynamical Systems and Parameters/2023-03-07 Misc.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Subscripts in $\LaTeX$
+$$
+v_max
+$$
+# $$
+# v_max
+# $$
+$$
+v_{max}
+$$
+# $$
+# v_{max}
+# $$
+$$
+v_{\rm max}
+$$
+# $$
+# v_{\rm max}
+# $$
+
+# how about in code?  you can't do subscripts
+
+# In[1]:
+
+
+v_max=5
+
+
+# In[2]:
+
+
+v_0=5
+
+
+# In[3]:
+
+
+v0=5
+
+
+# In[4]:
+
+
+vmax=5
+
+
+# In[ ]:
+
+
+
+
